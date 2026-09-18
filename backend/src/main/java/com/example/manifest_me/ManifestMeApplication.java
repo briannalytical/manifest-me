2eb3dd2b31253ed4a275cfb1715672d704a638ff
Brianna McNeil <brianna.c.mcneil@icloud.com>
2026-09-18 19:03:49 -0400

    initial commit

---
+++ backend/src/main/java/com/example/manifest_me/ManifestMeApplication.java
@@ -0,0 +1,13 @@
+package com.example.manifest_me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManifestMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManifestMeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/manifest_me/ManifestMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.manifest_me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManifestMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
